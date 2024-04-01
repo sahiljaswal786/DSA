ba1b65b8431fedd983a48a44ddae9457a4be156a
jaswalsahil786@gmail.com <Sahil.Jaswal@lntinfotech.com>
2024-04-01 13:22:36 +0530

    count subsets with sum = k

---
+++ DP/countSubsetSum.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int subsetSum (int index , int target,vector<int>& nums, vector<vector<int>>& dp ){
+    if ( target == 0){
+        return 1;
+    }
+
+    if ( index == 0 ){
+        return (nums[0] == target) ? 1 : 0;
+    }
+
+    if ( dp[index][target] != -1) return dp[index][target];
+
+    int noTaken = subsetSum (index - 1 , target, nums , dp);
+    int Taken = 0;
+    if(nums[index] <= target){
+        Taken = 1 + subsetSum(index - 1, target - nums[index] , nums , dp);
+    }
+
+    return dp[index][target] = Taken + noTaken;
+}
+
+int main(){
+    vector<int> nums = {1, 2, 2, 3};
+    int target = 3;
+
+    int n = nums.size();
+
+    vector<vector<int>> dp(n , vector<int> (target+1, -1));
+
+    cout<< subsetSum (n-1 , target, nums , dp);
+}
