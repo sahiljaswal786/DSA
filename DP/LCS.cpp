2b7f783505d45eb5313f2ad19f04017eeba15bca
jaswalsahil786@gmail.com <Sahil.Jaswal@lntinfotech.com>
2024-04-02 14:11:32 +0530

    longest common subsequence

---
+++ DP/LCS.cpp
@@ -0,0 +1,39 @@
+// lowest common subsequence
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int solve(int n, int m, vector<vector<int>> &dp, string &text1,
+          string &text2)
+{
+    if (n < 0 || m < 0)
+        return 0;
+
+    if (dp[n][m] != -1)
+        return dp[n][m];
+
+    if (text1[n] == text2[m])
+    {
+        return dp[n][m] = 1 + solve(n - 1, m - 1, dp, text1, text2);
+    }
+    else
+        return dp[n][m] = max(solve(n - 1, m, dp, text1, text2),
+                              solve(n, m - 1, dp, text1, text2));
+}
+int longestCommonSubsequence(string text1, string text2)
+{
+    int n = text1.size();
+    int m = text2.size();
+
+    vector<vector<int>> dp(n, vector<int>(m, -1));
+
+    return solve(n - 1, m - 1, dp, text1, text2);
+}
+
+int main()
+{
+    string text1 = "abcde";
+    string text2 = "ace";
+
+    cout << longestCommonSubsequence(text1, text2);
+}
